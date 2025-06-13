e8e80d812ff0cf36f55d8d7d308037068dd06e39
Pei Shi Yong <peishiyong@Peis-MacBook-Air-2024.local>
2025-06-14 08:48:30 +1000

    show editing input box when double clicked

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import type { TodoListProps } from '@/types';
 
 const { todolist } = defineProps<{
   todolist: TodoListProps
 }>()
 
-const emit = defineEmits(['toggle-complete', 'delete-item'])
+const activeEditId = ref<number | null>(null)
+
+const emit = defineEmits(['toggle-complete', 'delete-item', 'update-todo'])
 
 const toggleComplete = (itemId: number) => {
   emit('toggle-complete', itemId)
@@ -14,17 +17,32 @@ const toggleComplete = (itemId: number) => {
 const deleteItem = (itemId: number) => {
   emit('delete-item', itemId)
 }
+
+const editItem = (itemId: number) => {
+  activeEditId.value = itemId
+}
+
+const editedText = ref('')
+const updateItemText = (itemId: number) => {
+  const enteredText = editedText.value.trim()
+
+  emit('update-todo', itemId, enteredText)
+}
+const isEditing = (itemId: number) => activeEditId.value === itemId
+
 </script>
 
 <template>
   <section class="main">
     <ul class="todo-list">
-      <li v-for="item in todolist" :key="item.id" :class="{ completed: item.completed }">
+      <li v-for="item in todolist" :key="item.id" :class="{ completed: item.completed, editing: isEditing(item.id) }">
         <div class="view">
           <input class="toggle" type="checkbox" :checked="item.completed" @click="toggleComplete(item.id)" />
-          <label>{{ item.text }}</label>
+          <label @dblclick="editItem(item.id)">{{ item.text }}</label>
           <button class="destroy" @click="deleteItem(item.id)"></button>
         </div>
+        <input v-show="isEditing(item.id)" type="text" class="new-todo" :value="item.text" ref="editedText"
+          @keyup.enter="updateItemText(item.id)" />
       </li>
     </ul>
   </section>
