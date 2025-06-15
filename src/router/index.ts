0ea8ba1346e00ec85733d5a46a482af4abb3083f
Pei Shi Yong <peishiyong@Peis-MacBook-Air-2024.local>
2025-06-15 12:15:10 +1000

    does not seems to impact functionality

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,18 +7,16 @@ const routes = [
     path: '/',
     component: TodoApp,
     children: [
-      { path: '', name: 'all', key: 'all', component: TodoList, props: { filter: 'all' } },
+      { path: '', name: 'all', component: TodoList, props: { filter: 'all' } },
       {
         path: 'active',
         name: 'active',
-        key: 'active',
         component: TodoList,
         props: { filter: 'active' },
       },
       {
         path: 'completed',
         name: 'completed',
-        key: 'completed',
         component: TodoList,
         props: { filter: 'completed' },
       },
